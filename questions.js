5c7bf90f4382e2c3be65d68eb6df1b4819e13f39
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2013-07-16 22:29:23 +0900

    クラスっぽい感じにリファクタリング

---
+++ questions.js
@@ -0,0 +1,84 @@
+function Questions() {
+  var data = [
+    { // 0
+      name: "germany",
+      options: [
+        { optId: "1", option:"ドイツ" },
+        { optId: "2", option:"フランス" },
+        { optId: "3", option:"オランダ" }
+      ],
+      correctAnswer: "1"
+    },
+    { // 1
+      name: "uk",
+      options: [
+        { optId: "1", option:"ノルウェー" },
+        { optId: "2", option:"イギリス" },
+        { optId: "3", option:"グリーンランド" }
+      ],
+      correctAnswer: "2"
+    },
+    { // 2
+      name: "azerbaijan",
+      options: [
+        { optId: "1", option:"アゼルバイジャン" },
+        { optId: "2", option:"ルーマニア" },
+        { optId: "3", option:"ブルガリア" }
+      ],
+      correctAnswer: "1"
+    }
+  ],
+      idx = 0,
+      correctCnt = 0,
+      currentQuestion;
+  
+  this.nextQuestion = function() {
+    var nextData = data[idx];
+    if (nextData == undefined) {
+      return undefined;
+    }
+    currentQuestion = new Question(nextData, idx);
+    idx++;
+    return currentQuestion;
+  };
+  
+  this.getCorrectCnt = function() {
+    return correctCnt;
+  };
+
+  this.getTotalCnt = function() {
+    return idx;
+  };
+
+  this.isCorrect = function(selected) {
+    var correct = currentQuestion.isCorrect(selected);
+    if (correct) {
+      correctCnt++;      
+    }
+    return correct;
+  };
+
+  function Question(data, idx) {
+    var URL_PREFIX = "http://www.mofa.go.jp/mofaj/area/",
+        URL_SUFFIX = "/image/map.gif";
+    
+    this.idx = idx;
+    this.page = 'page' + idx;
+    this.btn = 'btn' + idx;
+    this.imageUrl = URL_PREFIX + data.name + URL_SUFFIX;
+    this.options = data.options;
+
+    this.checkOption = function(option) {
+      if (option < 0 || data.options.length < option) {
+        throw Error("illgal argument. [" + data.options + "] , " + option);
+      }
+    };
+
+    /** 正解の判定をする。*/
+    this.isCorrect = function(selected) {
+      return (selected == data.correctAnswer);
+    };
+
+    this.checkOption(data.correctAnswer);
+  }
+}
--- template.js
+++ template.js
@@ -1,83 +1,31 @@
+var questions = new Questions();
 
-var current;
 
-/** 次のページの情報を取得する。*/
-var getNextData = (function() {
-  
-  var i = 0;
-  var data = [
-    { // 0
-      name: "germany",
-      options: [
-        { optId: "1", option:"ドイツ" },
-        { optId: "2", option:"フランス" },
-        { optId: "3", option:"オランダ" }
-      ],
-      correctAnswer: "1"
-    },
-    { // 1
-      name: "uk",
-      options: [
-        { optId: "1", option:"ノルウェー" },
-        { optId: "2", option:"イギリス" },
-        { optId: "3", option:"グリーンランド" }
-      ],
-      correctAnswer: "2"
-    },
-    { // 2
-      name: "azerbaijan",
-      options: [
-        { optId: "1", option:"アゼルバイジャン" },
-        { optId: "2", option:"ルーマニア" },
-        { optId: "3", option:"ブルガリア" }
-      ],
-      correctAnswer: "1"
-    }
-  ];
-  return function() {
-    var URL_PREFIX = "http://www.mofa.go.jp/mofaj/area/",
-        URL_SUFFIX = "/image/map.gif",
-        ret = data[i];
-    
-    // 必要なプロパティを付与
-    if (ret !== undefined ) {
-      ret.page = 'page' + i;
-      ret.btn = 'btn' + i;
-      ret.imageUrl = URL_PREFIX + ret.name + URL_SUFFIX;
-      i += 1;
-    }
-    return ret;
-  };
-})();
-
-/** 次のページへ遷移する。*/
-var goNext = function() {
-  current = getNextData();
+/** 次のページへ遷移する。*/
+function goNext() {
+  var current = questions.nextQuestion();
   if (current == undefined) {
     alert('おしまい');
+    return;
   }
   
-  // bodyに新しいページを追加する。
+  // bodyに新しいページを追加する。
   $('#tmpl-page').tmpl(current).appendTo('body');
   
-  // ページ遷移を行う。
-  // ボタンにイベントを追加する。
-  $('#' + current.btn).click(function(event) {
-    alert(event.target.opt);
-    goNext();
-  });
-
   $.mobile.changePage('#' + current.page, {transition: 'flow'});
 
 };
 
 function verify(selected) {
-  if (!current) { return false; }
-  var msg = current.correctAnswer == selected ? '正解' : '不正解';
-  alert(msg);
+  var msg;
+  if (questions.isCorrect(selected)) {
+    msg = '正解';
+  } else {
+    msg = '不正解';
+  }
+  
+  alert(msg + ' : ' + questions.getCorrectCnt() + '/' + questions.getTotalCnt());
   goNext();
   return false;
 }
-
-
 goNext();
